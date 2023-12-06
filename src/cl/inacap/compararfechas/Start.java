0be17508daea4712510533efb0743603a0dd947c
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-06 10:43:19 -0300

    Ok

---
+++ src/cl/inacap/compararfechas/Start.java
@@ -0,0 +1,26 @@
+package cl.inacap.compararfechas;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Start {
+
+	public static void main(String[] args) {
+        try {
+            SimpleDateFormat sdformat = new SimpleDateFormat("dd/MM/yyyy");
+            Date fecha1 = sdformat.parse("05/10/2020");
+            Date fecha2 = sdformat.parse("05/10/2021");
+            System.out.println("Fecha-1: " + sdformat.format(fecha1));
+            System.out.println("Fecha-2: " + sdformat.format(fecha2));
+            if (fecha2.after(fecha1)) {
+                System.out.println("Fecha-2 es después que Fecha-1");
+            }
+            if (fecha1.equals(fecha2)) {
+            	System.out.println("Fecha-1 y Fecha-2 son iguales");
+            }
+        } catch(ParseException ex) {
+        	
+        }
+    }
+}   
